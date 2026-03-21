1333122f6292a1be42bbc089a668849c874ef3a3
AzureWings123 <raganvanessa@gmail.com>
2026-03-21 00:07:48 -0700

    Added to some controllers
    Since organizer and venue service haven't been made yet, the code in Organizer and VenueController is commented out for now so we can test other things.

--- ticketing/ticketing/src/main/java/com/example/ticketing/controller/OrganizerController.java
+++ ticketing/ticketing/src/main/java/com/example/ticketing/controller/OrganizerController.java
@@ -1,4 +1,25 @@
 package com.example.ticketing.controller;
 
+
+import com.example.ticketing.entity.Organizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/organizers")
 public class OrganizerController {
+
+    /* Uncomment after OrganizerService is made
+    @Autowired
+    private OrganizerService organizerService;
+
+    @PostMapping
+    public ResponseEntity<Organizer> createOrganizer(@RequestBody Organizer organizer){
+        Organizer created = organizerService.save(organizer);
+        return ResponseEntity.status(201).body(created);
+    } */
 }
--- ticketing/ticketing/src/main/java/com/example/ticketing/controller/VenueController.java
+++ ticketing/ticketing/src/main/java/com/example/ticketing/controller/VenueController.java
@@ -1,4 +1,26 @@
 package com.example.ticketing.controller;
 
+
+import com.example.ticketing.entity.Organizer;
+import com.example.ticketing.entity.Venue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/venues")
 public class VenueController {
+
+    /*
+    @Autowired
+    private VenueService venueService;
+
+    @PostMapping
+    public ResponseEntity<Venue> createVenue(@RequestBody Venue venue) {
+        Venue created = venueService.save(venue);
+        return ResponseEntity.status(201).body(created);
+    } */
 }
